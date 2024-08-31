41e8f59d5a0eb0d5e405cd639356257beda58b76
jangseyeong <jang.se.yeong000@gmail.com>
2024-09-01 02:50:46 +0900

    💬 comment: commitizen.sh 주석 추가 (#1)

--- commitizen.sh
+++ commitizen.sh
@@ -10,7 +10,8 @@ else
   echo "Warning: .husky/pre-commit file not found. Skipping pre-commit hook."
 fi
 
-# pre-commit 훅이 성공적으로 실행되었다면 Commitizen 실행
+# pre-commit 훅이 성공적으로 실행되었다면 Commitizen 실행 (commit-msg 체크 건너 뜀)
+# 직접 git commit 하는 경우가 있으니, 그때도 체크가 필요하기에 commit-msg 유지 필요
 if [ $? -eq 0 ]; then
   HUSKY=0 cz
 else
