e8283c50d3668f8f332915d039c0107e0db8853c
seyeong <137787915+JANGSEYEONG@users.noreply.github.com>
2024-09-20 16:24:18 +0900

    ✨ Feat/#33 탭 공용 컴포넌트 추가 (#43)
    * ✨ feat: shadcn/ui tab 추가 (#33)
    
    * ✨ feat: shadcn tabs 색상 커스텀, SlideBarTabs 컴포넌트 추가(active 탭 하단에 슬라이드 따라다님) (#33)

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -5,6 +5,8 @@ import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import { useTranslations } from 'next-intl';
 
+import SlideBarTabs from '@/components/common/tab/SlideBarTabs';
+
 export async function generateMetadata({
   params: { locale },
 }: {
@@ -20,10 +22,25 @@ export async function generateMetadata({
 // 메인 페이지
 export default function MainPage() {
   const t = useTranslations('main_page');
+
+  const tabs = [
+    {
+      value: '1',
+      label: '인기 글',
+      content: <div className="mt-2.5 w-full text-center">인기 글 내역</div>,
+    },
+    {
+      value: '2',
+      label: '인기 리뷰',
+      content: <div className="mt-2.5 w-full text-center">인기 리뷰 내역</div>,
+    },
+  ];
+
   return (
     <main id={DOM_IDS.CURRENT_SCROLL_PAGE} className="h-full w-full px-5 page-scrollable-container">
       <h1>{t('{nickName}님 안녕하세요', { nickName: 'tpdud' })}</h1>
-      <hr />
+
+      <SlideBarTabs tabs={tabs} defaultValue="1" />
       <h1 className="h-[3000px]">{t('{nickName}님 안녕하세요', { nickName: 'tpdud' })}</h1>
       <h2>블러어어</h2>
     </main>
---
+++ src/components/common/tab/SlideBarTabs.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { Loader2 } from 'lucide-react';
+import { useState, useEffect, useRef, useCallback } from 'react';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+
+interface SlideBarTabsProps {
+  tabs: { value: string; label: string; content: React.ReactNode }[];
+  defaultValue: string;
+}
+
+// Active 탭 하단을 따라다니는 슬라이드가 있는 탭
+const SlideBarTabs = ({ tabs, defaultValue }: SlideBarTabsProps) => {
+  const [activeTab, setActiveTab] = useState(defaultValue);
+  const [sliderStyle, setSliderStyle] = useState({});
+  const [isSliderLoading, setIsSliderLoading] = useState(true); // 슬라이드가 위치 찾을 때 까지 스피너 표시
+  const tabsListRef = useRef<HTMLDivElement>(null);
+
+  const updateSliderStyle = useCallback(() => {
+    // shadcn/ui는 Radix UI를 기반으로 하며, Radix UI에서는 현재 활성화된 탭에 'data-state="active"' 속성을 부여
+    // -> `active`로 슬라이더 위치 구할 탭 찾기
+    const activeElement = tabsListRef.current?.querySelector(
+      `[data-state="active"]`,
+    ) as HTMLElement;
+
+    if (activeElement) {
+      // 활성 탭의 위치와 너비를 기반으로 슬라이더 스타일을 업데이트
+      setSliderStyle({
+        left: `${activeElement.offsetLeft}px`,
+        width: `${activeElement.offsetWidth}px`,
+        transition: 'all 0.3s ease',
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      updateSliderStyle();
+    }, 0);
+
+    window.addEventListener('resize', updateSliderStyle);
+    setIsSliderLoading(false);
+    return () => {
+      clearTimeout(timerId);
+      window.removeEventListener('resize', updateSliderStyle);
+    };
+  }, [activeTab, updateSliderStyle]);
+
+  // #20240920.syjang, TODO: 임의로 Loader2 사용 -> 디자이너님이 스피너 정해주면 해당 스피너로 변경 예정
+  if (isSliderLoading) {
+    return (
+      <div className="flex h-40 items-center justify-center">
+        <Loader2 className="h-8 w-8 animate-spin text-orange-500" />
+      </div>
+    );
+  }
+
+  return (
+    <Tabs defaultValue={defaultValue} className="w-full" onValueChange={setActiveTab}>
+      <TabsList className="relative w-full flex-center" ref={tabsListRef}>
+        {tabs.map((tab) => (
+          <TabsTrigger key={tab.value} className="flex-1" value={tab.value}>
+            {tab.label}
+          </TabsTrigger>
+        ))}
+        <div className="absolute bottom-[-1.4px] h-0.5 bg-orange-500" style={sliderStyle} />
+      </TabsList>
+      {tabs.map((tab) => (
+        <TabsContent key={tab.value} value={tab.value}>
+          {tab.content}
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+};
+
+export default SlideBarTabs;
