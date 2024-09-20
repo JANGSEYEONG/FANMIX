f079b72c0b46f0ae717b3cb14aaf2dad5a15793d
seyeong <137787915+JANGSEYEONG@users.noreply.github.com>
2024-09-20 16:23:54 +0900

    ✨ Feat/#37 메시지박스 공용 컴포넌트 추가 (#41)
    ✨ feat: 메시지박스 공용 컴포넌트 추가 (#37)

---
+++ src/components/common/modal/MessageBox.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import { splitTextEvenly } from '@/lib/text';
+import { cn } from '@/lib/utils';
+import { useModalStore } from '@/stores/modalStore';
+
+// 액션 버튼 필요 요소 정의
+interface ActionButtonConfig {
+  text: string;
+  variant: 'primary' | 'secondary' | 'cancel';
+  onClick?: () => void;
+}
+
+interface MessageBoxProps {
+  title: string;
+  description?: string;
+  layout?: 'horizontal' | 'vertical';
+  buttons: ActionButtonConfig[];
+}
+
+const MessageBox = ({ title, description, layout = 'horizontal', buttons }: MessageBoxProps) => {
+  const hasDescription = !!description; // description이 존재하면 title, description 분리, 없으면 title이 곧 내용
+  const isHorizontal = layout === 'horizontal';
+  const buttonTotalLength = buttons.length;
+  return (
+    <div className="absolute left-0 top-0 z-20 h-full w-full flex-center">
+      <div className="w-[270px] animate-fadeIn border border-neutral-600 bg-neutral-900/90 opacity-0 blur-10">
+        <section className="gap-[2px] p-4 flex-col-center">
+          <h1
+            className={cn(
+              'whitespace-pre-wrap text-center text-white',
+              hasDescription ? 'body1-sb' : 'body1-m',
+            )}>
+            {title.length < 15 ? title : splitTextEvenly(title)}
+          </h1>
+          {hasDescription && (
+            <p className="overflow-wrap-anywhere whitespace-pre-wrap break-words text-center text-neutral-200 body3-r">
+              {description}
+            </p>
+          )}
+        </section>
+        <section
+          className={cn(
+            'w-full border-t-[0.5px] border-neutral-600 body1-sb',
+            isHorizontal ? 'flex' : 'flex-col-center',
+          )}>
+          {buttons.map((button, index) => (
+            // 마지막 버튼이 아닐 경우에 버튼 사이 구분선 그리기
+            <ActionButton
+              key={index}
+              {...button}
+              hasRightLine={isHorizontal && index != buttonTotalLength - 1}
+              hasBottomLine={!isHorizontal && index != buttonTotalLength - 1}
+            />
+          ))}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+interface ActionButtonProps extends ActionButtonConfig {
+  hasBottomLine?: boolean;
+  hasRightLine?: boolean;
+}
+const ActionButton = ({
+  text,
+  variant,
+  onClick,
+  hasBottomLine = false,
+  hasRightLine = false,
+}: ActionButtonProps) => {
+  const closeModal = useModalStore((state) => state.closeModal);
+  const getTextClass = (variant: ActionButtonConfig['variant']) => {
+    switch (variant) {
+      case 'primary':
+        return 'text-lime-400';
+      case 'secondary':
+        return 'text-orange-600';
+      case 'cancel':
+        return 'text-neutral-300 body1-m';
+      default:
+        return '';
+    }
+  };
+  const textClass = getTextClass(variant);
+  const handleClickButton = () => {
+    // 버튼 클릭 시, 모달을 닫고 콜백함수 실행
+    closeModal();
+    // callback이 있는 경우에만 실행
+    if (onClick) onClick();
+  };
+  return (
+    <button
+      onClick={handleClickButton}
+      className={cn(
+        'h-12 w-full transition-colors duration-150 flex-center hover:bg-neutral-950/80 focus:bg-black/50',
+        textClass,
+        hasBottomLine && 'border-b-[0.5px] border-neutral-600',
+        hasRightLine && 'border-r-[0.5px] border-neutral-600',
+      )}>
+      {text}
+    </button>
+  );
+};
+
+export default MessageBox;
